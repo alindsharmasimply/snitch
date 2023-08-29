e3cfdab109e4a4686b3150678c218958e0d6c816
alindsharmasimply <alindsharmasimply@gmail.com>
2023-08-29 21:57:31 +0530

    Project initialized with Clerk setup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import { ClerkProvider } from '@clerk/nextjs';
+import { Inter } from 'next/font/google';
+
+import '../globals.css';
+
+export const metadata = {
+  title: 'Snitch',
+  description: 'A Next.js 13 Snitch Application',
+};
+
+//Applies the font across all the files
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
