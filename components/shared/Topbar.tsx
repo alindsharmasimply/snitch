71330b61193996e6804470dd117aa4dfd492c154
alindsharmasimply <alindsharmasimply@gmail.com>
2023-08-31 21:34:56 +0530

    Added Left Sidebar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { SignedIn, SignOutButton, OrganizationSwitcher } from '@clerk/nextjs';
 
 // #FF7597
 export default function Topbar() {
@@ -9,6 +10,30 @@ export default function Topbar() {
         <Image src="/assets/logo.png" alt="logo" width={28} height={28} />
         <p className="text-heading2-bold text-light-1 max-xs:hidden">Snitch</p>
       </Link>
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: 'py-2 px-4',
+            },
+          }}
+        />
+      </div>
     </nav>
   );
 }
